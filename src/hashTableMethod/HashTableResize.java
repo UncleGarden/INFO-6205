f1934e17fd63d5886d7b935fabc47d1158f3339e
Jiateng <Jiateng@10.0.0.17>
2014-03-01 03:24:24 -0500

    wang.jiat

--- src/hashTableMethod/HashTableResize.java
+++ src/hashTableMethod/HashTableResize.java
@@ -5,16 +5,6 @@
  */
 package hashTableMethod;
 
-import hashMethod.APHash;
-import hashMethod.BKDRHash;
-import hashMethod.DEKHash;
-import hashMethod.DJBHash;
-import hashMethod.ELFHash;
-import hashMethod.JSHash;
-import hashMethod.PJWHash;
-import hashMethod.RSHash;
-import hashMethod.SDBMHash;
-
 /**
  *
  * @author Jiateng
@@ -23,8 +13,9 @@ public class HashTableResize {
 
     String[] table = new String[1];
     int step = 0;
+    int searchStep = 0;
     int count = 0;
-    float factor = (float) 0.5;
+    float factor = (float) 0.1;
     int x;
 
     public HashTableResize() {
@@ -33,42 +24,38 @@ public class HashTableResize {
     public void insert(String[] s) {
 
         for (int i = 0; i < s.length; i++) {
+
+            if (s[i] == null) {
+                continue;
+            }
+
             char[] c = s[i].toCharArray();
             //System.out.println((int) c[0]);
 
             switch ((int) c[0] % 9) {
                 case 1:
-                    x = APHash.APHash(s[i]);
+                    x = APHash(s[i]);
                 case 2:
-                    x = BKDRHash.BKDRHash(s[i]);
+                    x = BKDRHash(s[i]);
                 case 3:
-                    x = DEKHash.DEKHash(s[i]);
+                    x = DEKHash(s[i]);
                 case 4:
-                    x = DJBHash.DJBHash(s[i]);
+                    x = DJBHash(s[i]);
                 case 5:
-                    x = ELFHash.ELFHash(s[i]);
+                    x = ELFHash(s[i]);
                 case 6:
-                    x = JSHash.JSHash(s[i]);
+                    x = JSHash(s[i]);
                 case 7:
-                    x = PJWHash.PJWHash(s[i]);
+                    x = PJWHash(s[i]);
                 case 8:
-                    x = RSHash.RSHash(s[i]);
+                    x = RSHash(s[i]);
                 case 9:
-                    x = SDBMHash.SDBMHash(s[i]);
+                    x = SDBMHash(s[i]);
             }
 
-            System.out.println(x);
             insertAux(x, 0, s[i]);
         }
         System.out.println("Count:" + count);
-
-        //worst case
-//        for (int i = 0; i < s.length; i++) {
-//            int x = RSHash.RSHash(s[0]);
-//            System.out.println(x);
-//            insertAux(x, 0, s[i]);
-//        }
-//        System.out.println("Count:" + count);
     }
 
     public void insertAux(int x, int step, String str) {
@@ -89,10 +76,169 @@ public class HashTableResize {
     }
 
     public void resize(int capacity) {
+
         String[] copy = new String[capacity];
-        for (int i = 0; i < capacity / 2; i++) {
-            copy[i] = table[i];
-            table = copy;
+
+        String[] temp = new String[table.length];
+        for (int i = 0; i < table.length; i++) {
+            if (table[i] != null) {
+                temp[i] = table[i];
+            }
+            //temp = table;
         }
+        table = copy;
+        insert(temp);
+    }
+
+    public void search(String s) {
+
+        char[] c = s.toCharArray();
+
+        switch ((int) c[0] % 9) {
+            case 1:
+                x = APHash(s);
+            case 2:
+                x = BKDRHash(s);
+            case 3:
+                x = DEKHash(s);
+            case 4:
+                x = DJBHash(s);
+            case 5:
+                x = ELFHash(s);
+            case 6:
+                x = JSHash(s);
+            case 7:
+                x = PJWHash(s);
+            case 8:
+                x = RSHash(s);
+            case 9:
+                x = SDBMHash(s);
+        }
+
+        searchAux(x, 0, s);
+    }
+
+    public String searchAux(int x, int searchStep, String s) {
+
+        int position = x + searchStep;
+
+        int index = x;
+
+        if (index == (position + 1) % table.length) {
+            return null;
+        }
+
+        if (table[position % table.length] == s) {
+            System.out.println("Find key: " + x + " value: " + s);
+        } else {
+            searchAux(x, ++searchStep, s);
+        }
+        return null;
+    }
+
+    public int RSHash(String str) {
+
+        int a = 378551;
+        int b = 63689;
+        int hash = 0;
+        for (int i = 0; i < str.length(); i++) {
+            hash = hash * a + str.charAt(i);
+            hash = Math.abs(hash % table.length);
+            a = a * b;
+        }
+        return hash;
+    }
+
+    public int APHash(String str) {
+        int hash = 0xAAAAAAAA;
+        for (int i = 0; i < str.length(); i++) {
+            if ((i & 1) == 0) {
+                hash ^= ((hash << 7) ^ str.charAt(i) * (hash >> 3));
+            } else {
+                hash ^= (~((hash << 11) + str.charAt(i) ^ (hash >> 5)));
+            }
+            hash = Math.abs(hash % table.length);
+        }
+        return hash;
+    }
+
+    public int BKDRHash(String str) {
+        int seed = 131; // 31 131 1313 13131 131313 etc..  
+        int hash = 0;
+        for (int i = 0; i < str.length(); i++) {
+            hash = (hash * seed) + str.charAt(i);
+            hash = Math.abs(hash % table.length);
+        }
+        return hash;
+    }
+
+    public int DEKHash(String str) {
+        int hash = str.length();
+        for (int i = 0; i < str.length(); i++) {
+            hash = ((hash << 5) ^ (hash >> 27)) ^ str.charAt(i);
+            hash = Math.abs(hash % table.length);
+        }
+        return hash;
+    }
+
+    public int DJBHash(String str) {
+        int hash = 5381;
+        for (int i = 0; i < str.length(); i++) {
+            hash = ((hash << 5) + hash) + str.charAt(i);
+            hash = Math.abs(hash % table.length);
+        }
+        return hash;
+    }
+
+    public int ELFHash(String str) {
+        int hash = 0;
+        long x = 0;
+        for (int i = 0; i < str.length(); i++) {
+            hash = (hash << 4) + str.charAt(i);
+            if ((x = hash & 0xF0000000L) != 0) {
+                hash ^= (x >> 24);
+            }
+            hash &= ~x;
+            hash = Math.abs(hash % table.length);
+        }
+        return hash;
+    }
+
+    public int JSHash(String str) {
+
+        int hash = 1315423911;
+
+        for (int i = 0; i < str.length(); i++) {
+            hash ^= ((hash << 5) + str.charAt(i) + (hash >> 2));
+            hash = Math.abs(hash % table.length);
+        }
+        return hash;
+    }
+
+    public int PJWHash(String str) {
+
+        int BitsInUnsignedInt = (int) (4 * 8);
+        int ThreeQuarters = (int) ((BitsInUnsignedInt * 3) / 4);
+        int OneEighth = (int) (BitsInUnsignedInt / 8);
+        int HighBits = (int) (0xFFFFFFFF) << (BitsInUnsignedInt - OneEighth);
+        int hash = 0;
+        int test = 0;
+        for (int i = 0; i < str.length(); i++) {
+            hash = (hash << OneEighth) + str.charAt(i);
+            if ((test = hash & HighBits) != 0) {
+                hash = ((hash ^ (test >> ThreeQuarters)) & (~HighBits));
+            }
+            hash = Math.abs(hash % table.length);
+        }
+        return hash;
+    }
+
+    public int SDBMHash(String str) {
+        int hash = 0;
+        for (int i = 0; i < str.length(); i++) {
+            hash = str.charAt(i) + (hash << 6) + (hash << 16) - hash;
+            hash = Math.abs(hash % table.length);
+        }
+        return hash;
     }
 }
--- src/run/Main.java
+++ src/run/Main.java
@@ -6,9 +6,12 @@
 package run;
 
 import static generateArray.ArraySize.arraySize100;
+import static generateArray.ArraySize.arraySize1000;
+import static generateArray.ArraySize.arraySize2000;
 import hashTableMethod.HashTableNew;
 import hashTableMethod.HashTableResize;
 import java.io.IOException;
+import java.util.UUID;
 
 /**
  *
@@ -23,16 +26,33 @@ public class Main {
      */
     public static void main(String[] args) throws IOException {
 
-        String[] a = arraySize100();
+        //for worst case
+        String id = UUID.randomUUID().toString();
+
+        String[] a = arraySize2000();
         //String[] a = arraySize1000();
         //String[] a = arraySize2000();
-        //String[] a = arraySize5000();
-        //String[] a = arraySize10000();
 
+        long Start1 = System.nanoTime();
         HashTableResize htr = new HashTableResize();
         htr.insert(a);
+        long End1 = System.nanoTime();
+        System.out.println("Total Insert Time:" + (End1 - Start1));
 
-        //HashTableNew htn = new HashTableNew();
-        //htn.insert(a);
+        long SearchStart1 = System.nanoTime();
+        htr.search(id);
+        long SearchEnd1 = System.nanoTime();
+        System.out.println("Total Search Time:" + (SearchEnd1 - SearchStart1));
+
+        long Start2 = System.nanoTime();
+        HashTableNew htn = new HashTableNew();
+        htn.insert(a);
+        long End2 = System.nanoTime();
+        System.out.println("Total Insert Time:" + (End2 - Start2));
+
+        long SearchStart2 = System.nanoTime();
+        htr.search(id);
+        long SearchEnd2 = System.nanoTime();
+        System.out.println("Total Search Time:" + (SearchEnd2 - SearchStart2));
     }
 }
